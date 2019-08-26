30cac25dc8c43ecc44c7178073d0a0950fb0e2e2
Lukasz Szczepanski <lukasz.szczepanski@hemmersbach.com>
2019-08-26 14:00:03 +0200

    navigation between fragments implemented

--- app/src/main/java/com/example/animals/view/DetailFragment.kt
+++ app/src/main/java/com/example/animals/view/DetailFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.example.animals.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class DetailFragment : Fragment() {
 
@@ -19,5 +22,12 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        list_fab.setOnClickListener {
+            val action = DetailFragmentDirections.actionList()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
--- app/src/main/java/com/example/animals/view/ListFragment.kt
+++ app/src/main/java/com/example/animals/view/ListFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.animals.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -18,5 +20,12 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        detail_fab.setOnClickListener {
+            val action = ListFragmentDirections.actionDetail()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
